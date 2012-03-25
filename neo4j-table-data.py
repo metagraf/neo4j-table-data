e68b2f8d25b6a04b3327c7b042d611d55588687f
Thomas Reinholdsson <reinholdsson@gmail.com>
2012-03-25 15:28:07 +0200

    added getopt

--- neo4j-table-data.py
+++ neo4j-table-data.py
@@ -12,7 +12,40 @@
 # -
 
 from neo4j import GraphDatabase
-import csv
+import csv, getopt, sys
+
+def main():
+	''' The main method takes care of the option parameters, 
+	for more information on how getopt works 
+	please read http://www.doughellmann.com/PyMOTW/getopt/ '''
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "hi:o:v", ["help", "input=","output="])
+	except getopt.GetoptError, err:
+		# print help information and exit:
+		print str(err) # will print something like "option -a not recognized"
+		usage()
+		sys.exit(2)
+	output = None
+	verbose = False
+	for o, a in opts:
+		if o == "-v":
+			verbose = True
+		elif o in ("-h", "--help"):
+			usage()
+			sys.exit()
+		elif o in ("-i", "--input"):
+			input = a
+		elif o in ("-o", "--output"):
+			output = a
+		else:
+			assert False, "unhandled option"
+
+	print("Input file: " + str(input))
+	print("Output file: " + str(output))
+	
+if __name__ == "__main__":
+	main()
+
 
 
 
